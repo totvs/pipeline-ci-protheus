509226d60bdfb9703efd338e3f85cb04ad82adf8
Gabriel Alencar <pixcel97@gmail.com>
2023-10-17 18:16:53 -0300

    Adicionado exit code em caso de falha tir

--- tir/CRMA980TESTSUITE.py
+++ tir/CRMA980TESTSUITE.py
@@ -7,4 +7,7 @@ suite = unittest.TestSuite()
 suite.addTest(CRMA980('test_CRMA980_CT133'))
 
 runner = unittest.TextTestRunner(verbosity=2)
-runner.run(suite)
+result = runner.run(suite)
+
+if len(result.errors) > 0 or len(result.failures) > 0:
+    exit(1)
